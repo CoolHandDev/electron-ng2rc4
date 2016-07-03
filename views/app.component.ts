32df6d66a15ee9c2a080d187379dd9752aeeb2e5
CoolhandDev <mgshop1-github@yahoo.com>
2016-07-03 08:41:47 -0400

    add form1 component

--- views/app.component.ts
+++ views/app.component.ts
@@ -1,9 +1,13 @@
 import {Component} from '@angular/core';
+import {form1Component} from "./form1.component";
 
 @Component({
     selector: 'main-app',
     template: `
-            <h1>Angular 2 Release Candidate 4 is here!</h1>             
-`
+            <h1>Angular 2 Release Candidate 4 is here!</h1>
+            <br>
+            <form1></form1>
+            `,
+    directives: [form1Component]
 })
 export class MainComponent {}
---
+++ views/form1.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'form1',
+    templateUrl: './form1.html'
+})
+
+export class form1Component {
+
+}
