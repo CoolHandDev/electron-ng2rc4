a99d7571e799f64276fb6e5ce1cccfe22da7a4c0
CoolhandDev <mgshop1-github@yahoo.com>
2016-07-03 11:35:29 -0400

    change to one-way data binding

--- views/form1.component.ts
+++ views/form1.component.ts
@@ -8,4 +8,10 @@ import {Project} from '../models/project';
 
 export class form1Component {
     projectModel = new Project('', '');
+
+    onSubmit(form: any): void {
+        this.projectModel.name = form.name;
+        this.projectModel.type = form.type;
+        console.log('The form value is', form);
+    }
 }
