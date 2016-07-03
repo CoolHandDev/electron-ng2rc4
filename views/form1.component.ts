d86f98c3ae073bfde7cea14de2faa1003a90b290
CoolhandDev <mgshop1-github@yahoo.com>
2016-07-03 16:23:38 -0400

    add form validation

--- views/form1.component.ts
+++ views/form1.component.ts
@@ -4,7 +4,9 @@ import {
     FORM_DIRECTIVES,
     REACTIVE_FORM_DIRECTIVES,
     FormBuilder,
-    FormGroup
+    FormGroup,
+    Validators,
+    AbstractControl
 } from '@angular/forms';
 
 @Component({
@@ -16,16 +18,27 @@ import {
 export class form1Component {
     projectForm = FormGroup;
     projectModel = new Project('', '');
+    name: AbstractControl;
+    type: AbstractControl;
 
     constructor(fb: FormBuilder) {
-        this.projectForm = fb.group({'name': 'Default Project Name',
-                                    'type': 'Default Project Type'
+        this.projectForm = fb.group({
+            'name': ['', Validators.required],
+            'type': ['', Validators.required]
         });
+
+        this.name = this.projectForm.controls['name'];
+        this.type = this.projectForm.controls['type'];
     }
 
     onSubmit(form: any): void {
         this.projectModel.name = form.name;
         this.projectModel.type = form.type;
         console.log('The form value is', form);
+        if (this.projectForm.valid) {
+            console.log('form is valid');
+        } else {
+            console.log('form is not valid');
+        }
     }
 }
