09162c340ad37aac775bc67103c82436e8e32208
CoolhandDev <mgshop1-github@yahoo.com>
2016-07-02 21:04:45 -0400

    add semantic-ui

--- views/app.component.ts
+++ views/app.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'main-app',
-    template: '<h1>Angular 2 is here</h1>'
+    template: `
+            <h1>Angular 2 Release Candidate 4 is here!</h1>             
+`
 })
 export class MainComponent {}
