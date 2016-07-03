f4e285f538dc0a9e899cf9dea81089ee3bb1e259
CoolhandDev <mgshop1-github@yahoo.com>
2016-07-03 12:58:28 -0400

    use FormBuilder explicitly

--- views/form1.component.ts
+++ views/form1.component.ts
@@ -1,13 +1,27 @@
 import {Component} from '@angular/core';
 import {Project} from '../models/project';
+import {
+    FORM_DIRECTIVES,
+    REACTIVE_FORM_DIRECTIVES,
+    FormBuilder,
+    FormGroup
+} from '@angular/forms';
 
 @Component({
     selector: 'form1',
-    templateUrl: './form1.html'
+    templateUrl: './form1.html',
+    directives: [FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES]
 })
 
 export class form1Component {
+    projectForm = FormGroup;
     projectModel = new Project('', '');
+
+    constructor(fb: FormBuilder) {
+        this.projectForm = fb.group({'name': 'Default Project Name',
+                                    'type': 'Default Project Type'
+        });
+    }
 
     onSubmit(form: any): void {
         this.projectModel.name = form.name;
